815a2027372d3c7095d3e49d1a0f49ee772b540a
Lenya Hope <lehone4hope@gmail.com>
2019-04-05 11:09:44 +0100

    Restart audio when finished

--- app/src/main/java/com/example/lehone/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/lehone/musicplayer/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mPlayButton, mPauseButton;
+    private Button mPlayButton, mPauseButton, mResetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +16,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.p_12555214);
+        mediaPlayer.setLooping(true);
+
         mPlayButton = (Button) findViewById(R.id.button_play);
         mPauseButton = (Button) findViewById(R.id.button_pause);
+        mResetButton = (Button) findViewById(R.id.button_reset);
 
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // pause music
                 mediaPlayer.pause();
+            }
+        });
+
+        mResetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // reset music
+                mediaPlayer.pause();
+                mediaPlayer.seekTo(0);
+                mediaPlayer.start();
             }
         });
     }
